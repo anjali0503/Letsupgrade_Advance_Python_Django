40e9468607b94d4aab70ca760f3b2e62cd693dc3
Anjali Ramlolarakh Pandey <68697438+anjali0503@users.noreply.github.com>
2021-01-17 01:23:13 +0530

    Create Rolling_Dices.py

---
+++ Rolling_Dices.py
@@ -0,0 +1,10 @@
+import random
+min = 1
+max = 6
+roll_again = "yes"
+while roll_again = "yes" or roll_again = "y":
+    print("Rolling the dices....")
+    print("the values are...")
+    print(random.randit(min, max))
+    print(random.randit(min, max))
+    roll_again = ("Roll the dices again?")
